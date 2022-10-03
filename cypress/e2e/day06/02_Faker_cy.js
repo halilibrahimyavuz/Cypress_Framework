2fd36f9c6f8b06e89a89cf99e848d840d71e68d3
Halil Yavuz <halilibrahimyavuz07@gmail.com>
2022-10-03 23:31:25 +0300

    second

--- cypress/e2e/day06/02_Faker_cy.js
+++ cypress/e2e/day06/02_Faker_cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 import { faker } from '@faker-js/faker';
+
+
+
 /*
 Faker Kurulum
 1)https://www.npmjs.com/package/@faker-js/faker
@@ -7,28 +10,78 @@ Faker Kurulum
 3)import { faker } from '@faker-js/faker';  => clasin icinden import yapilmalidir 2. satira yazdik 1 defa import etmek tum classlarda calisir
 */
 describe('Using Faker', () => {
+
+
+
     it('Faker Login Test', () => {
+
+   
         cy.visit('http://automationpractice.com/index.php')
+
         cy.get('.login').click()
+
+       
+
         let email=faker.internet.email()//Ignacio29@gmail.com bunu olusturdu
           //let var const javascript degiskenleri isimlendirmede kullaniyoruz
+
+          cy.wait(3000)
          cy.get('#email_create').type(email)
+
          cy.get('#SubmitCreate > span').click()
-           cy.wait(7000)
+           cy.wait(13000)
          cy.get('#id_gender1').click()
+
+        
      let firstName=faker.name.firstName('male')//male erke ismi female kadin ismi verir
-     let lastname=faker.name.lastName('male')
+     
          cy.get('#customer_firstname').type(firstName)
+         let lastname=faker.name.lastName()
          cy.get('#customer_lastname').type(lastname)
-         cy.get('#email')
-         cy.get('#passwd')
-         cy.get('#firstname')
-         cy.get('#lastname')
-         cy.get('#address1')
-         cy.get('#city')
-         cy.get('#id_state').select(1)
-         cy.get('#id_state')
-         cy.get('#phone_mobile')
-         cy.get('#submitAccount > span')
+         
+         //cy.get('#email') email oto yaziyor burayi yazmadik gerekli kisimlari doldurcaz
+         
+         let password=faker.internet.password()
+         cy.get('#passwd').type(password)
+         cy.wait(2000) 
+         let address =faker.address.streetAddress()
+         cy.get('#address1').type(address)
+     
+         let city=faker.address.city() 
+         cy.get('#city').type(city)
+
+         cy.get('#id_state').select('1')
+
+         let zipcode=faker.address.zipCode('#####')
+         cy.get('#postcode').type(zipcode)
+
+         let tel=faker.phone.number('###-###-##-##')
+         cy.get('#phone_mobile').type(tel)
+
+        
+  
+         cy.get('#submitAccount > span').click()
+         cy.wait(6000)
+
+         cy.get('.account > span').should('include.text',firstName)
+         cy.get('.account > span').should('include.text',lastname)
+
+
+
+         
+
+
+    
+
+
+
+
+
+
+
+
     })
+
+   
+
 })
